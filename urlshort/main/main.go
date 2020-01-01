3cf691d585140804759617ab5b81d24807d82fc0
Brad Bartlett <hornet@shrike.hornetsnest.us>
2020-01-01 16:31:02 -0500

    load yaml from file

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gophercises/urlshort"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -18,18 +20,42 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlFile := flag.String("yaml", "urls.yaml", "a yaml file with the short urls")
+	flag.Parse()
+
+	yamlBytes, err := loadYaml(*yamlFile)
 	if err != nil {
 		panic(err)
 	}
+
+	yamlHandler, err := urlshort.YAMLHandler(yamlBytes, mapHandler)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func loadYaml(yamlFile string) ([]byte, error) {
+	file, err := os.Open(yamlFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := make([]byte, fileInfo.Size())
+	_, err = file.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer, nil
 }
 
 func defaultMux() *http.ServeMux {
